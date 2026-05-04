232c2a0c660800b25fd06eddbe1119d5a7133ab1
AshishG-21 <ashish.g_bca24-28@macfast.ac.in>
2026-05-04 16:32:07 +0530

    Update Mainrouter.jsx

--- Employee-room system/frontend/src/components/Mainrouter.jsx
+++ Employee-room system/frontend/src/components/Mainrouter.jsx
@@ -2,39 +2,38 @@ import React, { useState } from 'react'
 import Home from './pages/Home'
 import Employees from './pages/Employees'
 import EmployeesDetails from './pages/EmployeesDetails'
+import Employeesedit from './pages/Employeesedit'
 import Delete from './pages/Delete'
 import Delete2 from './pages/Delete2'
 import Delete3 from './pages/Delete3'
 import Rooms from './pages/Rooms'
 import RoomsDetails from './pages/RoomsDetails'
+import Roomsedit from './pages/Roomsedit'
 import Book from './pages/Book'
 import BookingDetails from './pages/BookingDetails'
-import { Routes, Route, Link } from 'react-router-dom'
+import Bookingedit from './pages/Bookingedit'
+import { Routes, Route, NavLink } from 'react-router-dom'
 import './App.css'
-
 const MainRouter = () => {
-  const [showEmployeesDropdown, setShowEmployeesDropdown] = useState(false)
-  const [showRoomsDropdown, setShowRoomsDropdown] = useState(false)
-  const [showBookingDropdown, setShowBookingDropdown] = useState(false)
 
   return (
     <div>
       
-      <nav class="bg-blue-600 text-white fixed top-0 w-full z-50 shadow-lg" style={{backgroundColor: "#1e3a8a"}}>
+      <nav className="bg-blue-600 text-white fixed top-0 w-full z-50 shadow-lg" style={{backgroundColor: "#1e3a8a",  position: 'sticky', top: 0, zIndex: 1000}}>
         <div className="container mx-auto px-4">
          
           
           <div className="flex justify-between items-center py-4">
             
             <div className="flex gap-6">
-             
-              <Link to="/" className="my-link" ><b>Home</b></Link>
+             <h1 className="room" style={{ textAlign: 'left' }}><b>RoomBooker</b></h1>
+              <NavLink to="/" className="my-link " ><b>Home</b></NavLink>
                 <span className="text-gray-400">{" | "}</span>
-             <Link to="/employees" className="my-link"><b>Employees</b></Link>
+             <NavLink to="/employees" className="my-link"><b>Employees</b></NavLink>
              <span className="text-gray-400">{" | "}</span>
-             <Link to="/rooms" className="my-link"><b>Rooms</b> </Link>
+             <NavLink to="/rooms" className="my-link"><b>Rooms</b> </NavLink>
              <span className="text-gray-400">{" | "}</span>
-            <Link to="/book" className="my-link"><b>Booking</b> </Link>
+            <NavLink to="/book" className="my-link"><b>Booking</b> </NavLink>
               
                
              
@@ -56,6 +55,9 @@ const MainRouter = () => {
           <Route path="/book" element={<Book />} />
           <Route path="/bookingdetails" element={<BookingDetails />} />
           <Route path="/delete3" element={<Delete3 />} />
+          <Route path="/employeesedit/:id" element={<Employeesedit />} />
+          <Route path="/roomsedit/:id" element={<Roomsedit />} />
+          <Route path="/bookingedit/:id" element={<Bookingedit />} />
         </Routes>
       </div>
     </div>
