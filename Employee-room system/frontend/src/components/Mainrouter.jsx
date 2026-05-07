4d6f1810d794568d542e7070490c3d2ae081d92d
AshishG-21 <ashish.g_bca24-28@macfast.ac.in>
2026-05-07 16:43:37 +0530

    Refactor MainRouter to remove unused components
    Removed unused imports and routes to streamline the MainRouter component.

--- Employee-room system/frontend/src/components/Mainrouter.jsx
+++ Employee-room system/frontend/src/components/Mainrouter.jsx
@@ -1,17 +1,8 @@
 import React, { useState } from 'react'
 import Home from './pages/Home'
 import Employees from './pages/Employees'
-import EmployeesDetails from './pages/EmployeesDetails'
-import Employeesedit from './pages/Employeesedit'
-import Delete from './pages/Delete'
-import Delete2 from './pages/Delete2'
-import Delete3 from './pages/Delete3'
 import Rooms from './pages/Rooms'
-import RoomsDetails from './pages/RoomsDetails'
-import Roomsedit from './pages/Roomsedit'
 import Book from './pages/Book'
-import BookingDetails from './pages/BookingDetails'
-import Bookingedit from './pages/Bookingedit'
 import { Routes, Route, NavLink } from 'react-router-dom'
 import './App.css'
 const MainRouter = () => {
@@ -26,14 +17,14 @@ const MainRouter = () => {
           <div className="flex justify-between items-center py-4">
             
             <div className="flex gap-6">
-             <h1 className="room" style={{ textAlign: 'left' }}><b>RoomBooker</b></h1>
-              <NavLink to="/" className="my-link " ><b>Home</b></NavLink>
-                <span className="text-gray-400">{" | "}</span>
-             <NavLink to="/employees" className="my-link"><b>Employees</b></NavLink>
-             <span className="text-gray-400">{" | "}</span>
-             <NavLink to="/rooms" className="my-link"><b>Rooms</b> </NavLink>
-             <span className="text-gray-400">{" | "}</span>
-            <NavLink to="/book" className="my-link"><b>Booking</b> </NavLink>
+             <h1 className="room" style={{ textAlign: 'left' , color: '#ffc107'}}><b>RoomBooker</b></h1>
+              <NavLink to="/" className="my-link " style={{marginRight: '30px'}}><b>Home</b></NavLink>
+                
+             <NavLink to="/employees" className="my-link" style={{marginRight: '30px'}}><b>Employees</b></NavLink>
+             
+             <NavLink to="/rooms" className="my-link" style={{marginRight: '30px'}}><b>Rooms</b> </NavLink>
+            
+            <NavLink to="/book" className="my-link" style={{marginRight: '30px'}}><b>Booking</b> </NavLink>
               
                
              
@@ -47,17 +38,8 @@ const MainRouter = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/employees" element={<Employees />} />
-          <Route path="/employeesdetails" element={<EmployeesDetails />} />
-          <Route path="/delete" element={<Delete />} />
           <Route path="/rooms" element={<Rooms />} />
-          <Route path="/roomsdetails" element={<RoomsDetails />} />
-          <Route path="/delete2" element={<Delete2 />} />
           <Route path="/book" element={<Book />} />
-          <Route path="/bookingdetails" element={<BookingDetails />} />
-          <Route path="/delete3" element={<Delete3 />} />
-          <Route path="/employeesedit/:id" element={<Employeesedit />} />
-          <Route path="/roomsedit/:id" element={<Roomsedit />} />
-          <Route path="/bookingedit/:id" element={<Bookingedit />} />
         </Routes>
       </div>
     </div>
